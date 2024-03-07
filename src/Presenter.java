451bdb285603f7dc47c85153141e498d1e0aaa65
Bastiaan Heinze <b.heinze@shopware.com>
2024-03-07 13:24:38 +0100

    added_actionlistener

--- src/Presenter.java
+++ src/Presenter.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Presenter {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Presenter implements ActionListener {
+    private Frontend frontend;
+    private PaketStation paketStation;
+    Presenter(){
+         frontend = new Frontend();
+         frontend.addActionListener(this);
+         paketStation = new PaketStation();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("Ende")){
+            System.out.println("Ende");
+        }
+    }
 }
